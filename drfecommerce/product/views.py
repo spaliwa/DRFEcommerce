cfa410ece7d70d8ee52a660b2d45ac71107da1a6
spaliwa <satyampaliwal11@gmail.com>
2024-05-27 19:53:42 +0530

    update View

--- drfecommerce/product/views.py
+++ drfecommerce/product/views.py
@@ -9,4 +9,24 @@ from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
 # Create your views here.
 
 class CategoryViewSer(viewsets.ViewSet):
-    pass
+    """
+    A simple Viewset for viewing all categories
+    """
+    queryset = Category.objects.all()
+    
+    @extend_schema(responses=CategorySerializer)
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+class BrandViewSet(viewsets.ViewSet):
+    """
+    A Simple Viewset for viewing all brands
+    """
+
+    queryset = Brand.objects.all()
+
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request):
+        serializer = BrandSerializer(self.queri=yset, many=True)
+        return Response(serializer.data)
