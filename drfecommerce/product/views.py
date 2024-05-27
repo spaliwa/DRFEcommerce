703c5f08c4e547dd8a2a2160c773953c38757a67
spaliwa <satyampaliwal11@gmail.com>
2024-05-27 19:29:19 +0530

    firsttCommit

---
+++ drfecommerce/product/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .models import Brand, Category, Product
+from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
+
+# Create your views here.
+
+class CategoryViewSer(viewsets.ViewSet):
+    pass
